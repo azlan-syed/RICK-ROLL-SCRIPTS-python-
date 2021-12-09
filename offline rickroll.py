e47abae0554b5efcd685f9e7784f605baf993150
Azlan-Syed <92157088+azlan-syed@users.noreply.github.com>
2021-12-09 10:00:43 +0530

    Update offline rickroll.py

--- offline rickroll.py
+++ offline rickroll.py
@@ -2,7 +2,7 @@ from playsound import playsound
 
 print("YOU GOT RICK ROLLED")
 
-playsound("Never_Gonna_Give_You_Up")
+playsound("Never_Gonna_Give_You_Up.mp3")
 
 # give the path of rick roll.mp3 and don't forget to do \\
 
